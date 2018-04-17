1dac98e02f30c0b2bd7c266d158e14c5b1141947
Rodrigo Pacheco Martinez Atienza <rpacheco@alpha49.aulas.gsyc.urjc.es>
2018-04-17 10:30:50 +0200

    Domain error

--- myproject/urls.py
+++ myproject/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'acorta.views.slash'),
-    url(r'^(\d+)$', 'acorta.views.number'),
-    url(r'.*', 'acorta.views.notfound'),
+    url(r'^$', 'cms.views.slash'),
+    url(r'^(\d+)$', 'cms.views.number'),
+    url(r'.*', 'cms.views.notfound'),
 )
