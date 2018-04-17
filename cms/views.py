e0bd257bcc43fc2531bb827304424e9c873982e7
Rodrigo Pacheco Martinez Atienza <rpacheco@alpha49.aulas.gsyc.urjc.es>
2018-04-17 10:38:01 +0200

    Exception included. Minor bug fix

--- cms/views.py
+++ cms/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from cms.models import Pages
 from django.http import HttpResponse
+from django.http import HttpResponseNotFound
 from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
 def slash(self):
     response = ''
-    for Page in Pages.objects.get():
+    for Page in Pages.objects.all():
         redirection = "<a href=/" + str(Page.id) + ">" + Page.name + "</a>"
-        response += (str(Page.id) + ' : ' + redirection + "/n"
+        response += (str(Page.id) + ' : ' + redirection + "/n")
     return(HttpResponse(response))
 
 
@@ -17,7 +18,8 @@ def number(self, num):
     try:
         Page = Pages.objects.get(id=str(num))
         return(HttpResponse(Page.page))
-
+    except ObjectDoesNotExist:
+        return(HttpResponse("Resource not in database"))
 
 def notfound(self):
-    return(HttpResponseNotFound("Resource not in database"))
+    return(HttpResponseNotFound("NOT FOUND"))
